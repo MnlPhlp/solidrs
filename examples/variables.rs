a3647f7b0e14d51af6cde76314df8fa6cf2e8df0
MnlPhlp <online@philipp-manuel.de>
2024-03-14 11:36:52 +0100

    fix clippy

--- examples/variables.rs
+++ examples/variables.rs
@@ -5,16 +5,16 @@ use crate::*;
 use solidrs::*;
 #[must_use]
 pub fn render() -> String {
-    var!(width, 10, "cube width");
-    var!(height, 20, "cube height");
-    let a = cube(width, width / 2, height).center();
+    var!(WIDTH, 10, "cube width");
+    var!(HEIGHT, 20, "cube height");
+    let a = cube(WIDTH, WIDTH / 2, HEIGHT).center();
     // you can save calculated points
     // they will still be displayed as the calculation when rendering.
     // this way changes to the variables in openscad apply everywhere
-    let cube_top = height / 2;
+    let cube_top = HEIGHT / 2;
     // if you use calc! the calculation will be stored to a variable in openscad
-    calc!(cylinder_height, width / 2);
-    let b = cylinder(cylinder_height, 5).translate(0, 0, cube_top);
+    calc!(CYLINDER_HEIGHT, WIDTH / 2);
+    let b = cylinder(CYLINDER_HEIGHT, 5).translate(0, 0, cube_top);
     let c = a + b;
     c.render_scad()
 }
